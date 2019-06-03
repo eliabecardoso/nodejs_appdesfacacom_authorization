6e79b1280b2f3c38d23f2690953be69d4570c64a
= <=>
2019-06-03 04:24:22 -0300

    migration e db config

---
+++ src/database/migrations/20190603052241-create_users.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        length: 100
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false
+      },
+      lastConnection: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("users");
+  }
+};
